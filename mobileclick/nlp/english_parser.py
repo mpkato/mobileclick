4ecde73e68669a8d8e0ffbd5ceafd8a56ad3f23c
mpkato <kato@dl.kuis.kyoto-u.ac.jp>
2015-06-25 18:28:06 +0900

    Add some functions to EnglishParser

--- mobileclick/nlp/english_parser.py
+++ mobileclick/nlp/english_parser.py
@@ -1,13 +1,48 @@
 # -*- coding: utf-8 -*-
 import nltk
+from nltk.corpus import stopwords
 
 class EnglishParser(object):
     def parse(self, sentence):
+        '''
+        Parse sentences and output pos-tagged tokens.
+        '''
         tokens = nltk.word_tokenize(sentence)
         tagged_tokens = nltk.pos_tag(tokens)
         return tagged_tokens
 
+    def noun_parse(self, sentence):
+        '''
+        Extract only nouns
+        '''
+        tagged_tokens = self.parse(sentence)
+        nouns = self.noun_filter(tagged_tokens)
+        nouns = self.stopword_filter(nouns, key=lambda x: x[0])
+        nouns = self.normalize(nouns, key=lambda x: x[0])
+        return nouns
+
+    def stopword_filter(self, tokens, key=lambda x: x):
+        '''
+        Filter out stopword tokens
+        '''
+        return [token for token in tokens
+            if not key(token) in stopwords.words("english")]
+
+    def noun_filter(self, tokens):
+        '''
+        Filter out non-noun tokens (pos starts with N)
+        '''
+        return [token for token in tokens
+            if token[1].startswith('N')]
+
+    def normalize(self, tokens, key=lambda x: x):
+        '''
+        Convert tokens to lowercase
+        '''
+        return [key(token).lower() for token in tokens]
 
 if __name__ == '__main__':
     ep = EnglishParser()
-    ep.parse("At 8 o'clock on Thursday morning Arthur didn't feel very good.")
+    tokens = ep.parse("At 8 o'clock on Thursday morning Arthur didn't feel very good.")
+    stopped = ep.stopword_filter(tokens, key=lambda x: x[0])
+    nouns = ep.noun_filter(stopped)
--- tests/english_parser_test.py
+++ tests/english_parser_test.py
@@ -3,7 +3,7 @@ import nose
 import unittest
 from mobileclick.nlp import EnglishParser
 
-class MethodTestCase(unittest.TestCase):
+class EnglishParserTestCase(unittest.TestCase):
 
     def setUp(self):
         self.ep = EnglishParser()
@@ -16,6 +16,35 @@ class MethodTestCase(unittest.TestCase):
         result = self.ep.parse(self.txt)
         self.assertEqual(result[0], ('At', 'IN'))
 
+    def test_english_parser_stopword_filter(self):
+        '''
+        EnglishParser.stopword_filter
+        '''
+        result = self.ep.stopword_filter(self.ep.parse(self.txt),
+            lambda x: x[0])
+        self.assertEqual(result[0], ('At', 'IN'))
+
+    def test_english_parser_noun_filter(self):
+        '''
+        EnglishParser.noun_filter
+        '''
+        result = self.ep.noun_filter(self.ep.parse(self.txt))
+        self.assertEqual(result[0], ('Thursday', 'NNP'))
+
+    def test_english_parser_normalize(self):
+        '''
+        EnglishParser.normalize
+        '''
+        result = self.ep.normalize(self.ep.parse(self.txt),
+            lambda x: x[0])
+        self.assertEqual(result[0], 'at')
+
+    def test_english_parser_noun_parse(self):
+        '''
+        EnglishParser.noun_parse
+        '''
+        result = self.ep.noun_parse(self.txt)
+        self.assertEqual(result[0], 'thursday')
 
 if __name__ == '__main__':
     nose.main(argv=['nose', '-v'])
