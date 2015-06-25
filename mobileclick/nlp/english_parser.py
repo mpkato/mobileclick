1ffa5dcf0c400709f635a469f62ee9ca471a60ca
mpkato <kato@dl.kuis.kyoto-u.ac.jp>
2015-06-26 00:48:08 +0900

    Add some light parsing methods to EnglishParser

--- mobileclick/nlp/english_parser.py
+++ mobileclick/nlp/english_parser.py
@@ -3,19 +3,35 @@ import nltk
 from nltk.corpus import stopwords
 
 class EnglishParser(object):
-    def parse(self, sentence):
+    def __init__(self):
+        self.stokenizer = nltk.tokenize.PunktSentenceTokenizer()
+        self.wtokenizer = nltk.tokenize.TreebankWordTokenizer()
+        self.tagger = nltk.data.load(nltk.tag._POS_TAGGER)
+
+    def word_tokenize(self, sentence):
+        '''
+        Fastest word tokenization
+        '''
+        tokens = [token for sent in self.stokenizer.tokenize(sentence)
+                    for token in self.wtokenizer.tokenize(sent)]
+        tokens = self.stopword_filter(tokens)
+        tokens = self.normalize(tokens)
+        return tokens
+
+    def pos_tokenize(self, sentence):
         '''
         Parse sentences and output pos-tagged tokens.
         '''
-        tokens = nltk.word_tokenize(sentence)
-        tagged_tokens = nltk.pos_tag(tokens)
+        tokens = [token for sent in self.stokenizer.tokenize(sentence)
+                    for token in self.wtokenizer.tokenize(sent)]
+        tagged_tokens = self.tagger.tag(tokens)
         return tagged_tokens
 
-    def noun_parse(self, sentence):
+    def noun_tokenize(self, sentence):
         '''
         Extract only nouns
         '''
-        tagged_tokens = self.parse(sentence)
+        tagged_tokens = self.pos_tokenize(sentence)
         nouns = self.noun_filter(tagged_tokens)
         nouns = self.stopword_filter(nouns, key=lambda x: x[0])
         nouns = self.normalize(nouns, key=lambda x: x[0])
--- tests/english_parser_test.py
+++ tests/english_parser_test.py
@@ -9,18 +9,18 @@ class EnglishParserTestCase(unittest.TestCase):
         self.ep = EnglishParser()
         self.txt = "At 8 o'clock on Thursday morning Arthur didn't feel very good."
 
-    def test_english_parser_parse(self):
+    def test_english_parser_pos_tokenize(self):
         '''
-        EnglishParser.parse
+        EnglishParser.pos_tokenize
         '''
-        result = self.ep.parse(self.txt)
+        result = self.ep.pos_tokenize(self.txt)
         self.assertEqual(result[0], ('At', 'IN'))
 
     def test_english_parser_stopword_filter(self):
         '''
         EnglishParser.stopword_filter
         '''
-        result = self.ep.stopword_filter(self.ep.parse(self.txt),
+        result = self.ep.stopword_filter(self.ep.pos_tokenize(self.txt),
             lambda x: x[0])
         self.assertEqual(result[0], ('At', 'IN'))
 
@@ -28,22 +28,22 @@ class EnglishParserTestCase(unittest.TestCase):
         '''
         EnglishParser.noun_filter
         '''
-        result = self.ep.noun_filter(self.ep.parse(self.txt))
+        result = self.ep.noun_filter(self.ep.pos_tokenize(self.txt))
         self.assertEqual(result[0], ('Thursday', 'NNP'))
 
     def test_english_parser_normalize(self):
         '''
         EnglishParser.normalize
         '''
-        result = self.ep.normalize(self.ep.parse(self.txt),
+        result = self.ep.normalize(self.ep.pos_tokenize(self.txt),
             lambda x: x[0])
         self.assertEqual(result[0], 'at')
 
-    def test_english_parser_noun_parse(self):
+    def test_english_parser_noun_tokenize(self):
         '''
         EnglishParser.noun_parse
         '''
-        result = self.ep.noun_parse(self.txt)
+        result = self.ep.noun_tokenize(self.txt)
         self.assertEqual(result[0], 'thursday')
 
 if __name__ == '__main__':
