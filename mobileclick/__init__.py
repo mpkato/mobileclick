799f9e7fa2dbeed96f4a978526c66e1dea19b97f
mpkato <kato@dl.kuis.kyoto-u.ac.jp>
2015-06-25 13:38:19 +0900

    Update mobileclick/__init__.py

---
+++ mobileclick/__init__.py
@@ -0,0 +1,6 @@
+from .index import Index
+from .index_factory import IndexFactory
+from .iunit import Iunit
+from .query import Query
+from .retrieval_run import RetrievalRun
+from .task import Task
