e607e7b6bd5a3c3c1b8cc639f53a9139298b7ab0
mpkato <kato@dl.kuis.kyoto-u.ac.jp>
2015-06-26 22:57:04 +0900

    Change download dir for mecab install

--- mecab_install.sh
+++ mecab_install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+# for Mac/Linux
 
 # Install mecab
-cd /var/tmp
+cd /tmp
 wget https://mecab.googlecode.com/files/mecab-0.996.tar.gz
 tar zxfv mecab-0.996.tar.gz
 cd mecab-0.996
@@ -14,10 +15,12 @@ sudo sh -c "echo '/usr/local/lib' >> /etc/ld.so.conf"
 sudo ldconfig
 
 # Install mecab-ipadic
-cd /var/tmp
+cd /tmp
 wget https://mecab.googlecode.com/files/mecab-ipadic-2.7.0-20070801.tar.gz
 tar zxfv mecab-ipadic-2.7.0-20070801.tar.gz
 cd mecab-ipadic-2.7.0-20070801
 ./configure --with-charset=utf8
 make
 sudo make install
+
+pip install mecab-python
