e5a33358fa6268a8db0a74376a249f4e6053dff8
mpkato <kato@dl.kuis.kyoto-u.ac.jp>
2015-06-26 18:05:14 +0900

    Add mecab_install.sh

---
+++ mecab_install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Install mecab
+cd /var/tmp
+wget https://mecab.googlecode.com/files/mecab-0.996.tar.gz
+tar zxfv mecab-0.996.tar.gz
+cd mecab-0.996
+./configure
+make
+sudo make install
+
+# load mecab.so
+sudo sh -c "echo '/usr/local/lib' >> /etc/ld.so.conf"
+sudo ldconfig
+
+# Install mecab-ipadic
+cd /var/tmp
+wget https://mecab.googlecode.com/files/mecab-ipadic-2.7.0-20070801.tar.gz
+tar zxfv mecab-ipadic-2.7.0-20070801.tar.gz
+cd mecab-ipadic-2.7.0-20070801
+./configure --with-charset=utf8
+make
+sudo make install
