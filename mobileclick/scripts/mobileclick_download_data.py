6486a2df3495532ad5cf1f47735025042f203dca
mpkato <kato@dl.kuis.kyoto-u.ac.jp>
2015-06-27 18:34:19 +0900

    Add post download message

--- mobileclick/scripts/mobileclick_download_data.py
+++ mobileclick/scripts/mobileclick_download_data.py
@@ -3,6 +3,23 @@
 MOBILECLICK_URL = 'http://www.mobileclick.org'
 DOCUMENT_A_TEXT = 'Document Collection (~ 1GB)'
 SUBSET_A_TEXT = 'Subset (~ 50MB)'
+POST_DOWNLOAD_MESSAGE = '''Finished downloading the data.
+Please find downloaded files at './data',
+and try the following commands for replicating the baseline results:
+
+mobileclick_random_ranking_method --runname random_ranking_method\
+--query data/MC2-training/en/1C2-E-queries.tsv\
+--iunit data/MC2-training/en/1C2-E-iunits.tsv\
+--indexdir data/MC2-training-documents/1C2-E.INDX\
+--pagedir data/MC2-training-documents/1C2-E.HTML
+
+mobileclick_lang_model_ranking_method --runname lang_model_ranking_method
+--query data/MC2-training/en/1C2-E-queries.tsv\
+--iunit data/MC2-training/en/1C2-E-iunits.tsv\
+--indexdir data/MC2-training-documents/1C2-E.INDX\
+--pagedir data/MC2-training-documents/1C2-E.HTML\
+--language english
+'''
 
 def main(istest=False):
     import sys, os, getpass
@@ -10,7 +27,8 @@ def main(istest=False):
     password = os.environ.get('MOBILECLICK_PASSWORD', None)
     if email is None or password is None:
         # if env variables are not set
-        print "Please input the email and password for %s" % MOBILECLICK_URL
+        print "Input your email and password for %s" % MOBILECLICK_URL
+        print "Please sign up if you haven't got them."
         sys.stdout.write('Email: ')
         email = raw_input()
         password = getpass.getpass()
@@ -19,6 +37,7 @@ def main(istest=False):
         for link in links:
             filename = download_file(link)
             deploy_data(filename)
+        print POST_DOWNLOAD_MESSAGE
     else:
         print "Login failed"
 
